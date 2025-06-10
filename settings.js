ce9a574ceb87ca877dcf82839fdd9e2531f0db2e
Noramila <mylaamaira@gmail.com>
2025-06-10 19:18:30 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "5OcxjQbS#RHEaFk45jq0fZxnN5mZM4FAUx5E732_Gzrv-FRTs_dQ",
 // add your Session Id make sure it starts with malvin~
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
